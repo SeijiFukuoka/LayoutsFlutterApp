1e1d923eae10caad81f4f6ac8b51e971416f0bd2
Seiji Fukuoka <seijifukuoka@gmail.com>
2019-09-08 21:22:49 -0300

    [FEAT] Add buttom row

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: <Widget>[
-        Expanded(
-          /*
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            /*
           * #1
           *
           * Putting a Column inside an Expanded widget stretches the column to
@@ -21,35 +23,34 @@ class MyApp extends StatelessWidget {
           * positions the column at the start of the row.
           *
           */
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              /* #2
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                /* #2
               *
               * Putting the first row of text inside a Container enables you to
               * add padding.
               * The second child in the Column, also text, displays as grey.
               *
               */
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Oeschinen Lake Campground',
+                Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      'Oeschinen Lake Campground',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+                Text(
+                  'Kandersteg, Switzerland',
                   style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                ),
-              )
-              ),
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(
-                  color: Colors.grey[500],
-                ),
-              )
-            ],
+                    color: Colors.grey[500],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        /* #3
+          /* #3
         *
         * The last two items in the title row are a star icon, painted red, and
         * the text “41”.
@@ -57,17 +58,28 @@ class MyApp extends StatelessWidget {
         * pixels.
         *
         * */
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41'),
-      ],
-    ),
-  );
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
 
-  @override
-  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layouts demo',
       home: Scaffold(
@@ -77,9 +89,31 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: <Widget>[
             titleSection,
+            buttonSection,
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
