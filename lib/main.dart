ac05f380af01196927f7d043063598fff846ac7a
Seiji Fukuoka <seijifukuoka@gmail.com>
2019-09-08 21:57:45 -0300

    [FEAT] Final touch with listview

--- lib/main.dart
+++ lib/main.dart
@@ -99,7 +99,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: <Widget>[
             Image.asset(
               'images/lake.jpeg',
