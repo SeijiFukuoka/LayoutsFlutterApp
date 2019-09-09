593ea071872a2894b7a745bdf8dd51c8c776b39b
Seiji Fukuoka <seijifukuoka@gmail.com>
2019-09-08 21:46:54 -0300

    [FEAT] Add image section

--- lib/main.dart
+++ lib/main.dart
@@ -101,6 +101,12 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
+            Image.asset(
+              'images/lake.jpeg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
             textSection,
